623efd96ddc3a3eaeb7b1a9f9f3e50de5f4cdd04
moerwald <andreas@mewald.com>
2019-10-18 13:16:22 +0200

    Added colors.

--- .githooks/pre-commit.ps1
+++ .githooks/pre-commit.ps1
@@ -1,4 +1,4 @@
-
+Write-HOST "KKKKKKKKK" -ForegroundColor Yellow
 $status = git status -s
 
 # Git status returns files to be commited with a 'M' right at the start of the line, files
@@ -7,9 +7,9 @@ $status = git status -s
 $status | Where-Object { ($_ -match "^M.*\.cs$") -or ($_ -match ".*.csproj") } | ForEach-Object {
 
 	& "$PScriptRoot/../build.ps1" -target compile
-	Write-Host "####################################"
+	Write-Host "####################################" -ForegroundColor Magenta
 	Write-Host ("make file returned: {0}" -f $LASTEXITCODE)
-	Write-Host "####################################"
+	Write-Host "####################################" -ForegroundColor Magenta
 	if ($LASTEXITCODE -ne 0) {
 		throw "It seems you code doesn't compile ... Fix compilation error(s) before commiting"
 	}
