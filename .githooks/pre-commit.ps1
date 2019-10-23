a188774194604ec302ce838bfb59a69b62f49eab
moerwald <andreas@mewald.com>
2019-10-23 19:04:50 +0200

    Initial version

--- .githooks/pre-commit.ps1
+++ .githooks/pre-commit.ps1
@@ -1,25 +1,5 @@
 $sourceFileSuffix = "cs"
 
-$status = git status -s
-
-# Check if the user may forgot to add files
-if ($status | Where-Object { $_ -match "^??\s(?<csFile>.*\.$sourceFileSuffix)" }) {
-	AddCommitInteractive -files $Matches.csFile -sourceCodeSuffix $sourceFileSuffix
-}
-
-# Git status returns files to be commited with a 'M' right at the start of the line, files
-# that have change BUT are not staged for commit are marked as ' M', notice the space at the
-# start of the line.
-if ($status | Where-Object { ($_ -match "^M.*\.$sourceFileSuffix$") -or ($_ -match ".*.csproj") }) {
-	& "$PScriptRoot/../build.ps1" -target compile
-	Write-Host "####################################" -ForegroundColor Magenta
-	Write-Host ("make file returned: {0}" -f $LASTEXITCODE)
-	Write-Host "####################################" -ForegroundColor Magenta
-	if ($LASTEXITCODE -ne 0) {
-		throw "It seems you code doesn't compile ... Fix compilation error(s) before commiting"
-	}
-}
-
 function AddCommitInteractive {
 	param(
 		[string]
@@ -31,7 +11,7 @@ function AddCommitInteractive {
 	Write-Host "It seems that you've unversioned $sourceCodeSuffix files" -ForegroundColor Green
 
 	$files | ForEach-Object {
-		AskForYesNo -text "Want to add the files to the GIT index (y/n)" `
+		AskForYesNo -text "Want to add the files to the GIT index" `
 				    -callBackAnswerIsYes { git add $_ } `
                     -callBackAnswerIsNo { 
 						AskForYesNo -text "Want to mark the file as 'assume unchanged'" `
@@ -58,8 +38,9 @@ function AskForYesNo {
 
 	# Keep asking until we've a valid user input
 	do {
-		$answer = (Read-Host -Prompt "$text ($yes/$no)").ToLower()
-	} while ( @($yes, $no) -contains $answer )
+		$answer = Read-Host -Prompt "$text ($yes/$no)"
+		Wait-Debugger
+	} while ( ($null -eq $answer) -or (@($yes, $no) -notcontains $answer.ToLower()) )
 
 	if ($answer -eq $yes) {
 		if ($callBackAnswerIsYes) {
@@ -70,5 +51,25 @@ function AskForYesNo {
 		if ($callBackAnswerIsNo) {
 			$callBackAnswerIsNo.Invoke()
 		}
+	}
+}
+
+$status = git status -s
+
+# Check if the user may forgot to add files
+if ($status | Where-Object { $_ -match "^??\s(?<csFile>.*\.$sourceFileSuffix)" }) {
+	AddCommitInteractive -files $Matches.csFile -sourceCodeSuffix $sourceFileSuffix
+}
+
+# Git status returns files to be commited with a 'M' right at the start of the line, files
+# that have change BUT are not staged for commit are marked as ' M', notice the space at the
+# start of the line.
+if ($status | Where-Object { ($_ -match "^M.*\.$sourceFileSuffix$") -or ($_ -match ".*.csproj") }) {
+	& "$PScriptRoot/../build.ps1" -target compile
+	Write-Host "####################################" -ForegroundColor Magenta
+	Write-Host ("make file returned: {0}" -f $LASTEXITCODE)
+	Write-Host "####################################" -ForegroundColor Magenta
+	if ($LASTEXITCODE -ne 0) {
+		throw "It seems you code doesn't compile ... Fix compilation error(s) before commiting"
 	}
 }
