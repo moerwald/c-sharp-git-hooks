874428a6393f03afed914d85188c12feec9ed7bf
moerwald <andreas@mewald.com>
2019-10-25 18:06:15 +0200

    pre-commit.ps1 didn't recognized added cs files.

--- .githooks/pre-commit.ps1
+++ .githooks/pre-commit.ps1
@@ -4,7 +4,7 @@ function Test-RelevantFileChanged {
 	# Git status returns files to be commited with a 'M' right at the start of the line, files
 	# that have change BUT are not staged for commit are marked as ' M', notice the space at the
 	# start of the line.
-	git status -s | Where-Object { ($_ -match "^M.*\.cs$") -or ($_ -match ".*.csproj") }
+	git status -s | Where-Object { ($_ -match ".*\.cs$") -or ($_ -match ".*\.csproj") }
 }
 
 function Start-Compile {
