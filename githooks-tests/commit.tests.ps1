75a64d8f18381064e9f419769dba0b59e17331f7
moerwald <andreas@mewald.com>
2019-10-27 11:19:17 +0100

    Moved stashing tests to own file. Removed Context statements, they're no longer needed.

--- githooks-tests/commit.tests.ps1
+++ githooks-tests/commit.tests.ps1
@@ -1,130 +1,37 @@
-
+. $PSScriptRoot/testRepoHelpers.ps1
 
 Describe "Test several commit scenarios" {
 
-    $defaultSourceFile = "Source.cs"
-    $defaultSourceFile2 = "Source2.cs"
+    $repositoryInfo = @{ }
 
-    function AssertCompileTargetWasCalled {
-        $buildScriptResultFile = "$env:TEMP/invoke-buildscript.json"
-        (Get-Content $buildScriptResultFile | ConvertFrom-Json).Target | Should -Be "compile"
-        $null = Remove-Item $buildScriptResultFile
-    }
-
-    function GetChangedFiles {
-        $changedFiles = "$env:TEMP/changedFiles.json"
-        Get-Content $changedFiles | ConvertFrom-Json
-        $null = Remove-Item $changedFiles
-    }
-
-    function ChangeAndCommitAlreadyIndexedFile {
-        "change" >> $defaultSourceFile
-        git add $defaultSourceFile
-        git commit -m "some change"
-    }
-
-    $guid = [System.Guid]::NewGuid()
-    $tempRepo = "$env:TEMP/$guid"
     BeforeEach {
         Push-Location
-        New-Item -ItemType Directory -Path $tempRepo
-
-        Set-Location $tempRepo
-
-        $githookDirectory = "$PSScriptRoot/../.githooks/"
-        $tmpRepoGitHookDirectory = "$tempRepo/.githooks"
-
-        Copy-Item -Path $githookDirectory  -Destination $tmpRepoGitHookDirectory -Recurse
-
-        @'
-function Invoke-BuildScript {
-    param($target)
-
-    Write-Host "Invoke-BuildScript with target $target called" -ForegroundColor Magenta
-    @{"Target" = $target} | ConvertTo-Json | Out-File "$env:TEMP/invoke-buildscript.json"
-
-}
-
-function Test-RelevantFileChanged {
-    param(
-        [Parameter(Mandatory, Position = 0)]
-        [string[]]
-        $changedFile
-    )
-
-    Write-Host "Test-RelevantFileChanged called. Change files: $($changedFile -join ',')" -ForegroundColor Magenta
-	$changedFile | Where-Object { ($_ -match ".*\.cs") } 
-    @{"ChangedFile" = $changedFile} | ConvertTo-Json | Out-File "$env:TEMP/changedFiles.json"
-}
-
-'@ | Out-File "$tmpRepoGitHookDirectory/functionsToInterfaceAgainst.ps1"
-
-        git init
-        "test" >> $defaultSourceFile
-        git add $defaultSourceFile
-        "test2" >> $defaultSourceFile2
-        git add $defaultSourceFile2
-        git add .githooks
-        git commit -m "Initial commit"
-        git config core.hooksPath $tmpRepoGitHookDirectory
+        $repositoryInfo = New-TestRepository 
     }
 
     AfterEach {
         Pop-Location
-        Remove-Item -Path $tempRepo -Recurse -Force
+        Remove-Item -Path $repositoryInfo.TempDirectory -Recurse -Force
     }
 
-
-    Context "Test if compile target is called" {
-        It "Already indexed source file changes" {
-            ChangeAndCommitAlreadyIndexedFile
-            AssertCompileTargetWasCalled
-        }
-
-        It "New source file is added" {
-            $newSourceFile = "new.cs"
-            "change" >> $newSourceFile
-            git add $newSourceFile
-            git commit -m "new file added"
-
-            AssertCompileTargetWasCalled
-        }
-
-        It "Already indexed source file is removed" {
-            git rm $defaultSourceFile
-            git commit -m "file removed"
-
-            AssertCompileTargetWasCalled
-        }
+    It "Already indexed source file changes" {
+        ChangeAndCommit-AlreadyIndexedFile -file $repositoryInfo.TestFile1
+        Assert-TargetWasCalled  -target "compile"
     }
 
-    Context "Stashing tests" {
-        It "Stash and restore untracked file" {
-            $someUntrackedFile = "untrackedFile"
-            "test" >> $someUntrackedFile
+    It "New source file is added" {
+        $newSourceFile = "new.cs"
+        "change" >> $newSourceFile
+        git add $newSourceFile
+        git commit -m "new file added"
 
-            ChangeAndCommitAlreadyIndexedFile
+        Assert-TargetWasCalled  -target "compile"
+    }
 
-            # Check if 
-            $changedFiles = (GetChangedFiles).ChangedFile 
-            $changedFiles | Should -Not -BeNullOrEmpty
-            $changedFiles | ForEach-Object {$_ | Should -Not -BeLike "*$someUntrackedFile*" }
-            Test-Path $someUntrackedFile | Should -BeTrue
-        }
+    It "Already indexed source file is removed" {
+        git rm $repositoryInfo.TestFile1
+        git commit -m "file removed"
 
-        It "Stash and restore changed but not staged file" {
-            $someUntrackedFile = "untrackedFile"
-            "test" >> $someUntrackedFile
-
-            ChangeAndCommitAlreadyIndexedFile
-
-            "change" >> $defaultSourceFile2
-
-            # Check if 
-            $changedFiles = (GetChangedFiles).ChangedFile 
-            $changedFiles | Should -Not -BeNullOrEmpty
-            $changedFiles | ForEach-Object {$_ | Should -Not -BeLike "*$defaultSourceFile2*" }
-            Test-Path $defaultSourceFile2 | Should -BeTrue
-        }
+        Assert-TargetWasCalled  -target "compile"
     }
 }
